80ba5fe6e4aa42907160842cce67bd1b0f11cdd5
MattSafi <matthewsafi798@gmail.com>
2024-06-13 21:31:28 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -112,9 +112,7 @@ function updateTally() {
 
   if (!hasWon && wins >= 10) {
     hasWon = true;
-    displayMessage(
-      "You reached 10 wins! You definitely get laid! What a Cool Cat! :D"
-    );
+    displayMessage("You reached 10 wins! What a Cool Cat! :D");
   } else if (!hasLost && losses >= 10) {
     hasLost = true;
     displayMessage("You reached 10 losses! Must suck to suck lmao!");
